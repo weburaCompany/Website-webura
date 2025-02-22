a401aab82ad2829dd50593740faeb474b7418a98
HamzaFerwana <hamza_ferwana@hotmail.com>
2025-02-22 23:49:47 +0200

    more fixes to site

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,8 +26,8 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            // 'email_verified_at' => now(),
+            'password' => '123456789',
             'remember_token' => Str::random(10),
         ];
     }
--- resources/views/site/contact-us.blade.php
+++ resources/views/site/contact-us.blade.php
@@ -1,6 +1,10 @@
 @extends('site.master')
-@section('title', 'Contact Us | ' .env('APP_NAME'))
-
+@section('title', 'Contact Us | ' . env('APP_NAME'))
+@section('styles')
+    <link rel="stylesheet" href="{{ asset('site-assets/css/contact-us.css') }}">
+    <link rel="stylesheet" href="{{ asset('site-assets/css/contact-us.css.map') }}">
+    <link rel="stylesheet" href="{{ asset('site-assets/css/contact-us.scss') }}">
+@endsection
 
 
 
@@ -13,18 +17,18 @@
         <div class="overlay"></div>
 
         <div class="container hero-info">
-          <h2>Welcome To Contact Page<span></span></h2>
-          <p>
-            Feel Work Together! Contact With us!
-          </p>
+            <h2>Welcome To Contact Page<span></span></h2>
+            <p>
+                Feel Work Together! Contact With us!
+            </p>
         </div>
 
 
 
-      </section>
-      <div class="header-icon">
-        <a data-mdb-ripple-init class="btn btn-success whatsapp" href="https://api.whatsapp.com/send/?phone=970592228451" target="_blank"
-            role="button" data-aos="fade-down" data-aos-delay="100" data-aos-duration="1000"><i
+    </section>
+    <div class="header-icon">
+        <a data-mdb-ripple-init class="btn btn-success whatsapp" href="https://api.whatsapp.com/send/?phone=970592228451"
+            target="_blank" role="button" data-aos="fade-down" data-aos-delay="100" data-aos-duration="1000"><i
                 class="fa-brands fa-whatsapp"></i></a> <br>
 
 
@@ -32,84 +36,83 @@
             data-aos-delay="100" data-aos-duration="1000"><i class="fa-solid fa-angles-up"></i></a>
     </div>
 
-<section class="contact-info" data-aos="fade-up">
-    <div class="container">
-        <div class="row">
+    <section class="contact-info" data-aos="fade-up">
+        <div class="container">
+            <div class="row">
 
 
-            <div class="col-sm-12 col-md-12 col-lg-12">
-                <h2 class="form-title">You Have Our Attention!</h2>
-                <p>Our team of certified engineers and tech enthusiasts is keen to help you
-                    securely & efficiently take your business to the next level.</p>
-                <div class="white-line"> </div>
-                <div class="d-flex flex-column flex-md-row justify-content-between business-info">
-                    <div class="business-email mb-3 mb-md-0">
-                        <strong>Business Email</strong><br>
-                        weburagaza@gmail.com
-                    </div>
-                    <div class="business-phone">
-                        <strong>Business Phone</strong><br>
-                        +970592228451
+                <div class="col-sm-12 col-md-12 col-lg-12">
+                    <h2 class="form-title">You Have Our Attention!</h2>
+                    <p>Our team of certified engineers and tech enthusiasts is keen to help you
+                        securely & efficiently take your business to the next level.</p>
+                    <div class="white-line"> </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between business-info">
+                        <div class="business-email mb-3 mb-md-0">
+                            <strong>Business Email</strong><br>
+                            weburagaza@gmail.com
+                        </div>
+                        <div class="business-phone">
+                            <strong>Business Phone</strong><br>
+                            +970592228451
+                        </div>
                     </div>
                 </div>
+
             </div>
-
         </div>
-    </div>
-</section>
+    </section>
 
-<section class="contact" data-aos="fade-up">
-    <div class="container">
-        <div class="row g-5">
-            <div class="contact-form">
-                <h3 class="form-title">Let's Connect!</h3>
-                <p class="form-subtitle">Send us a message, and we'll promptly discuss your project with you.</p>
-                <form>
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <i class="fas fa-user"></i>
-                                <input type="text" class="form-control" placeholder="Your Name" required>
+    <section class="contact" data-aos="fade-up">
+        <div class="container">
+            <div class="row g-5">
+                <div class="contact-form">
+                    <h3 class="form-title">Let's Connect!</h3>
+                    <p class="form-subtitle">Send us a message, and we'll promptly discuss your project with you.</p>
+                    <form>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <i class="fas fa-user"></i>
+                                    <input type="text" class="form-control" placeholder="Your Name" required>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <i class="fas fa-envelope"></i>
+                                    <input type="email" class="form-control" placeholder="Your Email" required>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <i class="fas fa-phone"></i>
+                                    <input type="tel" class="form-control" placeholder="Your Phone No." required>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <i class="fas fa-building"></i>
+                                    <input type="text" class="form-control" placeholder="Your Company Name">
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-group">
+                                    <i class="fas fa-message" style="top: 25px;"></i>
+                                    <textarea class="form-control" style="padding-bottom: 80px;" placeholder="How can we help you?" required></textarea>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <button type="submit" class="btn btn-submit">
+                                    Send Request
+                                    <i class="fas fa-arrow-right"></i>
+                                </button>
                             </div>
                         </div>
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <i class="fas fa-envelope"></i>
-                                <input type="email" class="form-control" placeholder="Your Email" required>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <i class="fas fa-phone"></i>
-                                <input type="tel" class="form-control" placeholder="Your Phone No." required>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <i class="fas fa-building"></i>
-                                <input type="text" class="form-control" placeholder="Your Company Name">
-                            </div>
-                        </div>
-                        <div class="col-12">
-                            <div class="form-group">
-                                <i class="fas fa-message" style="top: 25px;"></i>
-                                <textarea class="form-control" style="padding-bottom: 80px;"
-                                    placeholder="How can we help you?" required></textarea>
-                            </div>
-                        </div>
-                        <div class="col-12">
-                            <button type="submit" class="btn btn-submit">
-                                Send Request
-                                <i class="fas fa-arrow-right"></i>
-                            </button>
-                        </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
+
+
             </div>
-
-
         </div>
-    </div>
-</section>
+    </section>
 
 @endsection
